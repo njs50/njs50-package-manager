c30670151977ec631dd718f96d8b4dc6e2373823
Nick Shearer <nick@simianengineering.com>
2021-12-17 15:51:44 +1300

    adding the actual module

---
+++ config.lua
@@ -0,0 +1,5 @@
+mpackage = [[njs50-package-manager]]
+author = [[njs50]]
+title = [[manage tfe packages]]
+description = [[manage packages for the forests edge mud]]
+created = "2021-12-17T14:56:11+13:00"
